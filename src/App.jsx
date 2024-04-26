4adcb99c17373f6cb2dc39dae10c02c5ab7c13ba
Mmesoma Maryann <89715739+Maryann-01@users.noreply.github.com>
2024-04-26 15:39:21 +0000

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mmesoma Amaraizu",
+  title: "Frontend Web Developer",
+  email: "amaraizummesoma@gmail.com",
+  gitHub: "Maryann-01",
+  instagram: "soma_maryann",
+  linkedIn: "Mmesoma Amaraizu",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "soma_maryann",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
